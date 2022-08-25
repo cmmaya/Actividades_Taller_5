c03e55026c79c5bbbab06b8a3d30a5f58ad652ed
cmmaya <cmonsalvem@unal.edu.co>
2022-08-25 23:44:51 +0300

    Puntos 5-8
    desarrollo puntos 5 al 8

--- EdadHexBin/Src/main.c
+++ EdadHexBin/Src/main.c
@@ -4,7 +4,7 @@
 //////PUNTO 1/////////
 
 
-main() {
+main(void) {
    unsigned short dias_transcurridos = 0;
    unsigned int   horas_transcurridos = 0;
    unsigned int   segundos_transcurridos = 0;
@@ -39,24 +39,22 @@ main() {
    printf("\n","");
 
  /////PUNTO 5////////
-
- /* Para transformar el numero a binario, primero inicializo una matriz de 32 espacios y las variables n e i que vamos a utilizar en el for
- *Luego en un ciclo for, almaceno progresivamente el residuo de el numero inicial dividido por 2
- *Repito el ciclo con el numero dividido entre 2 hasta que ya no se pueda dividir */
-
    int i,n,h,k;
    int a[13],b[18],c[7];
+ /* Para transformar el numero a binario almaceno progresivamente el residuo de el numero inicial dividido por 2 hasta que ya no se pueda dividir
+ * para poder ver el numero en el debugger lo almaceno en una matriz a[]*/
+
+
    n = dias_transcurridos;
    for(i=0;n>0;i++)
    {
    a[i]=n%2;
    n=n/2;
-   /*Imprimo cada elemento de la matriz concatenado empezando desde el final hasta el principio*/
    }
-   for(i=i-1;i>=0;i--)
-   {
-   printf("%d",a[i]);
-   }
+   /*anoto el numero resultante dias_binario= 1111101001000*/
+   unsigned long dias_binario= 1111101001000;
+
+
 
    /*hacemos lo mismo para las horas transcurridas, usando la matriz b[]*/
       printf("\n","");
@@ -66,12 +64,10 @@ main() {
    {
    b[i]=h%2;
    h=h/2;
-   /*Imprimo cada elemento de la matriz b[] desde el ultimo elemento hasta el primero*/
    }
-   for(i=i-1;i>=0;i--)
-   {
-   printf("%d",b[i]);
-   }
+   /*anoto el numero resultante tambien
+   horas_binario=101110111011000101*/
+
 
       /*Para obtener el hexadecimal guardamos en la matriz c[] los modulos del numero divido por 16 hasta que ya no se pueda seguir dividiendo*/
    printf("\n","");
@@ -80,12 +76,41 @@ main() {
    {
    c[i]=k%16;
    k=k/16;
-   /*Imprimo cada elemento de la matriz c[] desde el ultimo elemento hasta el primero*/
    }
-   for(i=i-1;i>=0;i--)
-   {
-   printf("%d",c[i]);
-   }
-}
+      /*El resultado es 1 11 8 2 10 13 0
+   reemplazando los numeros por sus correspondientes letras queda
+   1B82AD0*/
 
- /////PUNTO 6////////
+
+/////PUNTO 6////////
+   dias_transcurridos=dias_transcurridos<<1;
+   dias_transcurridos=dias_transcurridos<<1;
+   dias_transcurridos=dias_transcurridos<<1;
+   dias_transcurridos=dias_transcurridos<<1;
+   /*explicacion: Al aplicar la operacion es como multiplicar por 2 el valor inicial, al seguir ejecutando la operacion el valor se multiplica por 2^n. 4 veces equivale a multiplicar por 16*/
+/////PUNTO 7////////
+   horas_transcurridos=horas_transcurridos>>1;
+   horas_transcurridos=horas_transcurridos>>1;
+   horas_transcurridos=horas_transcurridos>>1;
+   horas_transcurridos=horas_transcurridos>>1;
+   /*explicacion: Al aplicar la operacion es como dividir por 2 el valor inicial, al seguir ejecutando la operacion el valor se divide por 2^n. 4 veces equivale a dividir por 16*/
+
+/////PUNTO 8////////
+   /*retomo el valor original de la variable dias_transcurridos*/
+   dias_transcurridos = 8008;
+   /*Lo invertimos: bitwise NOT*/
+   dias_transcurridos= ~dias_transcurridos;
+   /*sumamos 1 */
+   dias_transcurridos= dias_transcurridos+1;
+   /*sumamos el valor original*/
+   dias_transcurridos= dias_transcurridos+8008;
+
+  /*EL RESULTADO DA 0 PARA CUALQUIER VARIABLE*/
+
+
+/////PUNTO 9////////
+
+  /*El numero de la 4ta posicio
+
+   printf("%d", dias_transcurridos);
+}
