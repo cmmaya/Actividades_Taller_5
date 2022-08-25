f59418c73fb9e2742e6e345a645c2f7ccbda7f44
cmmaya <cmonsalvem@unal.edu.co>
2022-08-25 18:32:59 +0300

    Puntos 2,3 y 4 completos
    Se realizaron los puntos 2 al 4 del taller 1

---
+++ Clase2/Src/main.c
@@ -0,0 +1,14 @@
+
+#include <stdint.h>
+
+
+//si no defino unsigned me la cuenta con signo
+unsigned char SiempreChar;
+
+//definicion de variable de 8 bits sin signo
+uint8_t dias;
+
+//complemento a2, para represntar numeros negativos
+int main(void){
+	while(1){}
+}
--- EdadHexBin/Src/main.c
+++ EdadHexBin/Src/main.c
@@ -1,53 +1,91 @@
-/*
- * main.c
- *
- *  Created on: Aug 20, 2022
- *      Author: Camilo
- */
 #include <stdint.h>
 #include<stdio.h>
 #include<stdlib.h>
+//////PUNTO 1/////////
 
-   short dias_transcurridos;
-   int   horas_transcurridos;
-   int   segundos_transcurridos;
 
 main() {
-	   short years  =  2022;
-	   char months =  9;
-	   char days   =  10;
-	   char horas  =  5;
+   unsigned short dias_transcurridos = 0;
+   unsigned int   horas_transcurridos = 0;
+   unsigned int   segundos_transcurridos = 0;
+   unsigned short years  =  2022;
+   unsigned char months =  9;
+   unsigned char days   =  10;
+   unsigned char horas  =  5;
 
     /* El numero de dias entre 2001 y 2021 = (years-1-2000)*365
      * El numero de dias en el 2000 = (12-months+1)*31-(days)
      * El numero de dias transcurridos hasta el 7 de agosto =(7)*31+ (31-17)
      * No se cuenta el ultimo dia, pues esta incompleto*/
 
+
+/////PUNTO 2////////
    dias_transcurridos = ((years-1-2000)*365+(12-months)*31+(30-days) + (7)*31+ (31-17))-1;
+
+
+/////PUNTO 3////////
    /*para las horas multiplico los dias por 24 y sumo las horas del dia que falta*/
    horas_transcurridos = dias_transcurridos*24 + horas;
+
+/////PUNTO 4////////
    /*para los segundos multiplico las horas por 3600*/
    segundos_transcurridos = dias_transcurridos*3600 + horas*3600;
 
+
+/*imprimo los valores*/
    printf("%d",dias_transcurridos);
    printf("\n%d",horas_transcurridos);
    printf("\n%d",segundos_transcurridos);
    printf("\n","");
 
+ /////PUNTO 5////////
 
  /* Para transformar el numero a binario, primero inicializo una matriz de 32 espacios y las variables n e i que vamos a utilizar en el for
  *Luego en un ciclo for, almaceno progresivamente el residuo de el numero inicial dividido por 2
  *Repito el ciclo con el numero dividido entre 2 hasta que ya no se pueda dividir */
-   int a[32],i,n;
+
+   int i,n,h,k;
+   int a[13],b[18],c[7];
    n = dias_transcurridos;
    for(i=0;n>0;i++)
    {
    a[i]=n%2;
    n=n/2;
-   /*Imprimo cada elemento de la matriz concatenado*/
+   /*Imprimo cada elemento de la matriz concatenado empezando desde el final hasta el principio*/
    }
    for(i=i-1;i>=0;i--)
    {
    printf("%d",a[i]);
    }
+
+   /*hacemos lo mismo para las horas transcurridas, usando la matriz b[]*/
+      printf("\n","");
+   h = horas_transcurridos;
+
+   for(i=0;h>0;i++)
+   {
+   b[i]=h%2;
+   h=h/2;
+   /*Imprimo cada elemento de la matriz b[] desde el ultimo elemento hasta el primero*/
    }
+   for(i=i-1;i>=0;i--)
+   {
+   printf("%d",b[i]);
+   }
+
+      /*Para obtener el hexadecimal guardamos en la matriz c[] los modulos del numero divido por 16 hasta que ya no se pueda seguir dividiendo*/
+   printf("\n","");
+   k = segundos_transcurridos;
+   for(i=0;k>0;i++)
+   {
+   c[i]=k%16;
+   k=k/16;
+   /*Imprimo cada elemento de la matriz c[] desde el ultimo elemento hasta el primero*/
+   }
+   for(i=i-1;i>=0;i--)
+   {
+   printf("%d",c[i]);
+   }
+}
+
+ /////PUNTO 6////////
